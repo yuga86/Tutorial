c1d65d30bbdc9296950be393a00400abdba0d8a0
yuga86 <yuga86@gmail.com>
2016-08-20 08:34:58 -0700

    My Poc Application
    Just for leaning

---
+++ MyApplication/app/src/main/java/com/example/aayangoud/myapplication/SignUp.java
@@ -0,0 +1,24 @@
+package com.example.aayangoud.myapplication;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Aayan Goud on 8/20/2016.
+ */
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+    }
+
+}
